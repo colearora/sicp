087478085d4118176f120ef2844b8908b14f08f4
Cole Arora <cole.arora@outlook.com>
2020-07-14 00:44:32 -0500

    Fix 1.39

--- ch1/sicp-ex-1.39.scm
+++ ch1/sicp-ex-1.39.scm
@@ -7,7 +7,7 @@
 ; tan-cf returns an approximation to tan(x) using
 ; Lambert's continued fraction expansion.
 (define (tan-cf x k)
-  (cont-frac (lambda (i) (fast-expt x i))
+  (cont-frac (lambda (i) (if (= i 1) x (square x)))
              (lambda (i) (+ (* 2 (- i 1)) 1))
              -
              k))
@@ -23,14 +23,6 @@
                  (combiner (d i) res)))))
   (iter k 0.0))
 
-; fast-expt returns the nth power of b in O(log n) time.
-(define (fast-expt b n)
-  (define (iter a b n)
-    (cond ((= n 0) a)
-          ((even? n) (iter a (square b) (/ n 2)))
-          (else (iter (* a b) b (- n 1)))))
-  (iter 1 b n))
-
 (define (square x)
   (* x x))
  
@@ -39,11 +31,13 @@
 ;===============================================================================
 
 (tan-cf 0.00 100) ; 0.0
-(tan-cf 0.25 100) ; 0.25532481426449716
-(tan-cf 0.50 100) ; 0.5458723128641024
-(tan-cf 0.75 100) ; 0.9293417567017108
+(tan-cf 0.25 100) ; 0.25534192122103627
+(tan-cf 0.50 100) ; 0.5463024898437905
+(tan-cf 0.75 100) ; 0.9315964599440725
 (tan-cf 1.00 100) ; 1.557407724654902
+(tan-cf 1.25 100) ; 3.009569673862831
+(tan-cf 1.50 100) ; 14.101419947171719
+(tan-cf 1.55 100) ; 48.078482479219396
+(tan-cf 1.57 100) ; 1255.7655915008268
 
-; Note: tan-cf becomes increasingly inaccurate as input
-; x becomes increasingly larger than 1.
 
